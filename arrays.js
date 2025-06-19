615744f7ffdd218355d76e7a90672507a830618d
pranav2005-m <maniruddhapranav@gmail.com>
2025-06-19 11:54:54 +0530

    arrays

---
+++ arrays.js
@@ -0,0 +1,23 @@
+let veg=["carrot","beans","tomato","potato"];
+console.log("Original array");
+console.log(veg);
+console.log("Elements of the array");
+
+veg.push("onion");
+console.log(veg);
+console.log("Elements of array after pushing onion")
+
+veg.pop();
+console.log(veg);
+console.log("Elements of array after pop the final element")
+
+console.log(veg[0]);
+console.log(veg.length);
+
+veg.unshift("brinjal");
+console.log(veg)
+console.log("unshift");
+
+veg.shift("potato");
+console.log(veg)
+console.log("shift ele")
