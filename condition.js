d2f016a15b69447e8d770be365bb492c784da393
pranav2005-m <maniruddhapranav@gmail.com>
2025-06-19 11:21:05 +0530

    if else condition

---
+++ condition.js
@@ -0,0 +1,28 @@
+let a=49
+if(a>=50){
+    console.log("Pass")
+}
+else{
+    console.log("fail")
+}
+
+let n=99
+if(n>98){
+    console.log("grade -o")
+}
+else if(n>80){
+    console.log("grade -A")
+}
+else if(n>=50){
+    console.log("grade -B")
+}
+else{
+    console.log("fail")
+}
+
+if(1){
+    console.log("on")
+}
+else{
+    console.log("off")
+}
