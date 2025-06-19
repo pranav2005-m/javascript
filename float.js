aa2a1c1ad89819c1ae100d79a685cdc4fcf56352
pranav2005-m <maniruddhapranav@gmail.com>
2025-06-19 11:03:23 +0530

    assignment operators

---
+++ float.js
@@ -0,0 +1,14 @@
+let m=0.2;
+let n=2.3;
+console.log(m+n);
+let r=Math.round((234533+234)/10).toFixed(2);
+console.log(r);
+console.log(2<3||3>2)
+console.log(3==4)
+console.log(5>=6)
+console.log("3"===3)
+console.log("5"==5)
+console.log(12*2)
+console.log(3/2)
+console.log(2+2)
+console.log(6-2)
