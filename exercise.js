00bebc871e493a0e1548f537e0e4b8a92d1617ca
pranav2005-m <maniruddhapranav@gmail.com>
2025-06-19 13:39:55 +0530

    javascript cose exersise

---
+++ exercise.js
@@ -0,0 +1,8 @@
+let n = [5, 12, 8, 20, 3, 15];
+for (let i = 0; i < n.length; i++) {
+  if (n[i] > 10) {
+    console.log(n[i] + " is greater than 10");
+  } else {
+    console.log(n[i] + " is 10 or less");
+  }
+}
