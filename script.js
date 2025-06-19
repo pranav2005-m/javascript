ef7e5645397a0a9aedc5d4e9723d7b26be38a889
pranav2005-m <maniruddhapranav@gmail.com>
2025-06-19 10:01:19 +0530

    add script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+console.log("i am anil from mdk ");
+console.log("2"+2);
+
+console.log("2"-2);
+let a="dinesh";
+let b="dharshini";
+console.log(a+b);
+let c=1;
+let d=3;
+let e=8;
+if(c>d>e){
+    console.log(c);
+}
+else{
+    console.log(e);
+}
